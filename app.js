0de0cbd9ebf7e3edc9ab27d5a029e8563fa9f662
Amruth <ammumandappa@gmail.com>
2025-01-29 20:15:30 +0530

    assigned players

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const socket = require("socket.io");
 const http = require("http");
 const { Chess } = require("chess.js");
-const path = require('path')
+const path = require("path");
 const app = express(); //  for routing part and middleware
 const server = http.createServer(app); // http server should be based on express server
 
@@ -14,16 +14,30 @@ let currentPlayer = "W";
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res)=> {
-    res.render("index", {title: "Chess Game"})
-})
+app.get("/", (req, res) => {
+  res.render("index", { title: "Chess Game" });
+});
 
-io.on("connection", (socket)=> {
-    console.log("A new client connected: " + socket.id);
+io.on("connection", (socket) => {
+  console.log("A new client connected: " + socket.id);
 
-    socket.on("disconnect",()=> {
-        console.log("Client disconnected");
-    } )
-})
+  if (!players.white) {
+    players.white = socket.id;
+    socket.emit("playerRole", "W");
+  } else if (!players.black) {
+    players.black = socket.id;
+    socket.emit("playerRole", "B");
+  } else {
+    socket.emit("spectatorRole");
+  }
 
-server.listen(3000)
+  socket.on("disconnect", () => {
+    if (socket.id === players.white) {
+      delete players.white;
+    } else if (socket.id === players.black) {
+      delete players.black;
+    }
+  });
+});
+
+server.listen(3000);
